e66ff59d49cece0b3d8a865221148f52acf2f8ef
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-10-28 21:43:42 +0500

    Started htrml makin

---
+++ rnow/src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import SubjectNow from "./subjectNow";
+import Subjects from "./subjects";
+
+ReactDOM.render(
+    <div>
+        <App/>
+        <Subjects/>
+    </div>,
+    document.getElementById("root")
+);
---
+++ rnow/src/subjectNow.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+class SubjectNow extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+
+            </React.Fragment>
+        );
+    }
+}
+
+export default SubjectNow;
