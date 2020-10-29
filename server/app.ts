9078bb916959aa559917867e7682edbf1ccfd8fb
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-10-29 14:16:48 +0500

    Made POST file writing

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,25 @@
 import { debug } from 'console';
 import express, { request, response } from 'express';
+import cors from "cors";
+import bodyParser from 'body-parser';
+
 const fs = require("fs");
 
 const app = express();
 const port = 6969; 
 
+app.use(cors())
 app.get('/', (request, response) => {
     response.send("Hi motherfucker");
 })
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+const PASSWORD = 1359;
 
 app.get("/class/:id", (req, res) => {
     try{
@@ -19,6 +31,23 @@ app.get("/class/:id", (req, res) => {
     catch{         
         const data = {"code" : "fail"};
         res.json(data); return;
+    }
+});
+
+app.post("/class/add", (req, res) => {
+    try{
+        if (req.body.password != PASSWORD){
+            res.json({ "code" : "bad password" }); 
+            return;
+        }
+        console.log(req.body);
+        console.log(req.body.newTimeTable);
+        const fileToEdit = fs.writeFileSync(`${req.body["class"]}.json`, JSON.stringify(req.body.newTimeTable, null, 2));
+        res.json(req.body["class"]);
+    }
+    catch (e){
+        res.send(e.toString());
+        console.log(e);
     }
 });
 
